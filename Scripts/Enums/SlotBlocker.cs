70cf04f20087ae8cacd97daf04bb23c1345b0592
MiNiMaL027 <andriy.boichuk@oski.site>
2024-06-26 21:30:31 +0300

    Update visual

---
+++ Scripts/Enums/SlotBlocker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Enums
+{
+    [Flags]
+    public enum SlotBlocker
+    {
+        None = 0,
+        Cooldown = 1 << 1,
+        EnergyOut = 1 << 2
+    }
+}
--- Scripts/Widgets/Inventory/BattlefieldBattlePlantSlot.cs
+++ Scripts/Widgets/Inventory/BattlefieldBattlePlantSlot.cs
@@ -3,37 +3,36 @@ using Enums;
 using Godot;
 using Items;
 using System;
-using System.ComponentModel;
-using Widgets.ContextMenu;
-using Widgets.ToolTip;
 
 namespace Widgets.Inventory
 {
     public partial class BattlefieldBattlePlantSlot : BaseSlot
     {
-        public bool IsBlocked = false;
         Item item;
         ShaderMaterial material;
         Label LabelEnergy;
         Timer TimerBlockSlot;
         BattlePlantDataBaseRow battlePlantDataBaseRow;
+        HBoxContainer EnergyContainer;
+
+        SlotBlocker SlotBlockers = SlotBlocker.None;
         public override void _Ready()
         {
             SetProcess(false);
             TextureRect = GetNode<TextureRect>("Panel/TextureRect");
             LabelAmount = GetNode<Label>("Panel/LabelAmount");
+            EnergyContainer = GetNode<HBoxContainer>("Panel/EnergyContainer");
             material = (ShaderMaterial)GetNode<ColorRect>("Panel/ShaderColorRect").Material;
             TimerBlockSlot = GetNode<Timer>("TimerBlockSlot");
             TimerBlockSlot.Timeout += TimerBlockSlot_Timeout;
-            LabelEnergy = GetNode<Label>("HBoxContainer/LabelEnergy");
             base._Ready();
         }
 
         private void TimerBlockSlot_Timeout()
         {
-            IsBlocked = false;
-            SetProcess(false);
+            SlotBlockers &= ~SlotBlocker.Cooldown;
 
+            SetProcess(false);
         }
 
         public override void _Process(double delta)
@@ -43,16 +42,30 @@ namespace Widgets.Inventory
         public override void _GuiInput(InputEvent e)
         {
             base._GuiInput(e);
-            if (e is InputEventMouseButton mouseButton && mouseButton.ButtonIndex == MouseButton.Left && mouseButton.IsPressed() == true && IsBlocked == false)
+            if (e is InputEventMouseButton mouseButton && mouseButton.ButtonIndex == MouseButton.Left && mouseButton.IsPressed() == true)
             {
                 PlayerController playerController = this.GetPlayerController();
-                ItemType itemType = DbService.GetItemType(battlePlantDataBaseRow.Id);
-                item = Item.GetItemSceneByType(itemType);
-                if (playerController.BattlefieldEnergy < battlePlantDataBaseRow.EnergyToPlace)
+                GD.Print(SlotBlockers);
+                if (SlotBlockers != SlotBlocker.None)
                 {
-                    playerController.Hud.MainWidget.InfoWindow.AddInfoPanel($"Required {battlePlantDataBaseRow.EnergyToPlace} energy to use plant");
+                    Disable(1, new Color(0.608f, 0.118f, 0, 0.5f), new Color(1, 1, 1, 1));
+
+                    if ((SlotBlockers & SlotBlocker.EnergyOut) == SlotBlocker.EnergyOut)
+                    {
+                        playerController.Hud.MainWidget.InfoWindow.AddInfoPanel($"Required {battlePlantDataBaseRow.EnergyToPlace} energy to use plant", battlePlantDataBaseRow.TextureSpritePath);
+                    }
+
+                    if ((SlotBlockers & SlotBlocker.Cooldown) == SlotBlocker.Cooldown)
+                    {
+                        playerController.Hud.MainWidget.InfoWindow.AddInfoPanel($"Plant on cooldown", battlePlantDataBaseRow.TextureSpritePath);
+                    }
+
                     return;
                 }
+            
+                ItemType itemType = DbService.GetItemType(battlePlantDataBaseRow.Id);
+                item = Item.GetItemSceneByType(itemType);
+                
                 playerController.BattlefieldEnergy -= battlePlantDataBaseRow.EnergyToPlace;
 
                 ///spawn item in world and make it current pressed object
@@ -87,22 +100,48 @@ namespace Widgets.Inventory
             base.Init(item, amountToSet);
             battlePlantDataBaseRow = (BattlePlantDataBaseRow)item;
             TimerBlockSlot.WaitTime = battlePlantDataBaseRow.PlacementCooldown;
-            LabelEnergy.Text = battlePlantDataBaseRow.EnergyToPlace.ToString();
-            this.GetPlayerController().EnergyUpdated += BattlefieldBattlePlantSlot_EnergyUpdated;
+            var energyTexture = ResourceLoader.Load<Texture2D>("res://raw assets/Images/Hud/Energy_full.png");
+            
+            EnergyContainer.RemoveChildren();
+            for (int i = 0; i < battlePlantDataBaseRow.EnergyToPlace; i++)
+            {
+                var textureRect = new TextureRect() { Texture = energyTexture, ExpandMode = TextureRect.ExpandModeEnum.FitWidthProportional };
+                EnergyContainer.AddChild(textureRect);
+            }
+
+            BattlefieldBattlePlantSlot_EnergyUpdated(this.GetPlayerController().BattlefieldEnergy);
+            this.GetPlayerController().EnergyUpdated += BattlefieldBattlePlantSlot_EnergyUpdated;           
         }
 
         private void BattlefieldBattlePlantSlot_EnergyUpdated(int newEnergy)
         {
-            if (newEnergy >= battlePlantDataBaseRow.EnergyToPlace)
-            {
-                LabelEnergy.Set("theme_override_colors/font_color", Color.Color8(255, 255, 255));
+            EnergyContainer.RemoveChildren();
+            var yellowEnergy = battlePlantDataBaseRow.EnergyToPlace;
+            var redEnergy = Math.Max(battlePlantDataBaseRow.EnergyToPlace - newEnergy, 0);
+            yellowEnergy -= redEnergy;
+            var energyTexture = ResourceLoader.Load<Texture2D>("res://raw assets/Images/Hud/Energy_full.png");
 
-            }
-            else 
+            for (int i = 0; i < yellowEnergy; i++)
             {
-                LabelEnergy.Set("theme_override_colors/font_color", Color.Color8(255, 0, 0));
+                var textureRect = new TextureRect() { Texture = energyTexture, ExpandMode = TextureRect.ExpandModeEnum.FitWidthProportional };
+                EnergyContainer.AddChild(textureRect);
             }
+            for (int i = 0; i < redEnergy; i++)
+            {
+                var textureRect = new TextureRect() { Texture = energyTexture, ExpandMode = TextureRect.ExpandModeEnum.FitWidthProportional, Modulate = new Color(0.608f, 0.09f, 0) };
+                EnergyContainer.AddChild(textureRect);
+            }
+
+            if (newEnergy < battlePlantDataBaseRow.EnergyToPlace)
+            {
+                SlotBlockers |= SlotBlocker.EnergyOut;
+            }
+            else
+            {
+                SlotBlockers &= ~SlotBlocker.EnergyOut;
+            }         
         }
+
         public override void _ExitTree()
         {
             base._ExitTree();
@@ -113,7 +152,14 @@ namespace Widgets.Inventory
         {
             TimerBlockSlot.Start();
             SetProcess(true);
-            IsBlocked = true;
+            SlotBlockers |= SlotBlocker.Cooldown;
+        }
+
+        private void Disable(int disabledTime, Color disableColor, Color defaultColor)
+        {
+            Tween tween = CreateTween();
+            Modulate = disableColor;
+            tween.TweenProperty(this, "modulate", defaultColor, disabledTime);
         }
     }
 }
